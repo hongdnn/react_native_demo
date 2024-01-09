1f8a0ceb2efa686462c5c7e10a40196641cf0163
hongdnn <namhongdoan99@gmail.com>
2024-01-09 13:27:29 +0700

    add react-native-paper

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
 import store from './redux/store';
+import { PaperProvider } from 'react-native-paper';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
+      <PaperProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </PaperProvider>
     </Provider>
   );
 };
